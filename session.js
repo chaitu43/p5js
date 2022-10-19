d1fdcd111d55d7e77a18fdff0fa27fdbd3a5829d
chaitanya pranav <101542958+chaitu43@users.noreply.github.com>
2022-10-19 18:45:43 +0530

    Add files via upload

---
+++ session.js
@@ -0,0 +1,15 @@
+function setup() {
+    createCanvas(800, 600);
+    background('pink');
+  }
+  
+  function draw() {
+    rectMode(CENTER);
+    stroke(255,0,0);
+    fill('violet');
+    strokeWeight(20);
+    rect(200,115,60,50);
+    line(120,180,80,75)
+    /* rect(x axis, y axis, lenth,breadth)*/
+
+  }
